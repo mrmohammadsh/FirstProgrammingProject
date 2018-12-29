0176133475e323f90861a31d5c651a113ed72fb8
sahand <sahandzoufan79@gmail.com>
2018-12-30 01:34:14 +0330

    reading customers info bug is dont read any text file with tab correctly

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ typedef unsigned long long int ulint;
 //------------------------------------------NameSpace--------------------------------------------
 using namespace std;
 //-----------------------------------------GlobalVariables&Classes---------------------------------------
-fstream Cities,Accounts;    //Class for reading file
+fstream Cities,Accounts,Customers;    //Class for reading file
 struct City{
     int X,Y;
 };//CountryCitys[300];
 struct Account{
     lint ID,Password,Mony;
 };
+struct Customer{
+    string username,Password,Firstname,Lastname,Mobile,Friend,BId,Bpassword;
+};
+vector<struct Customer> CustomerList;
 vector<struct City> CountryCitys;
 vector<struct Account> CountryAccount;
 //CountryCitys.resize(300,0);
@@ -23,13 +27,17 @@ vector<struct Account> CountryAccount;
 
 void OpenFile(){
     cout<<"Reading Cities....";
-    Cities.open("cities.txt");
+    Cities.open("cities.txt",ios::in|ios::app);
     if(Cities.is_open()){
         cout<<"Reading Accounts";
-        Accounts.open("accounts.txt");
+        Accounts.open("accounts.txt",ios::in|ios::app);
         if(Accounts.is_open()){
+            Customers.open("Customers.txt",ios::in|ios::app);
+            if(Customers.is_open()){
             cout<<"Reading File was Succesful :)";
             return;
+            }
+            
         }
         
     }
@@ -39,6 +47,7 @@ void OpenFile(){
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 void InsertData(){
     int temp,index;
+    string tmp;
     while(Cities>>index){
         CountryCitys.push_back(City());
         Cities>>temp;
@@ -53,7 +62,32 @@ void InsertData(){
         CountryAccount[CountryAccount.size()-1].Password=temp;
         Cities>>temp;
         CountryAccount[CountryAccount.size()-1].Mony=temp;
+         
     }
+    while(Customers>>tmp){
+        CustomerList.push_back(Customer());
+        CustomerList[CustomerList.size()-1].BId=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Bpassword=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Firstname=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Lastname=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].username=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Password=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Mobile=tmp;
+        Customers>>tmp;
+        CustomerList[CustomerList.size()-1].Friend=tmp;
+    }
+}
+void CloseFiles(){
+    Cities.close();
+    Accounts.close();
+    Customers.close();
+
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -62,5 +96,7 @@ int main(){
     
     OpenFile();
     InsertData();
+    CloseFiles();
+   cout<<CountryAccount[3].Mony;
     return 0;
 }
